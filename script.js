30a0a7052189b96450ca34ce3ced51b07f28d8f1
Samarth Agarwal <88262012+Samarth-001@users.noreply.github.com>
2021-12-12 12:00:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,17 @@ const btnscroll4 = document.getElementById("btnscroll4");
 const btnscroll5 = document.getElementById("btnscroll5");
 
 btnscroll1.addEventListener("click", function() {
-   window.scrollTo({
-       top=500,
-       left=0,
-   })
+   window.scrollBy({top:970, behavior:'smooth'})
+});
+btnscroll2.addEventListener("click", function() {
+   window.scrollBy({top:970, behavior:'smooth'})
+});
+btnscroll3.addEventListener("click", function() {
+   window.scrollBy({top:1900, behavior:'smooth'})
+});
+btnscroll4.addEventListener("click", function() {
+   window.scrollBy({top:2890, behavior:'smooth'})
+});
+btnscroll5.addEventListener("click", function() {
+   window.scrollBy({top:3720, behavior:'smooth'})
 });
